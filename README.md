ace570c5741c6776b6d4408b1aa60c5c99ed21cd
Gilbert Pellegrom <gilbert@pellegrom.me>
2021-12-09 13:47:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lemon Squeezy Theme Schema
+
+This repository provides a schema for the Lemon Squeezy `theme.json` file as well as a CLI tool to generate a `theme.json` from a theme.
+
+```
+ls-theme-generate
+```
---
+++ bin/generate.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const yargs = require('yargs');
+const glob = require('glob');
+const chalk = require('chalk');
+const compiler = require('vue-template-compiler');
+const moduleFromString = require('module-from-string');
+
+(async () => {
+    await yargs
+        .scriptName('ls-theme-generate')
+        .command('$0', 'Generate a Lemon Squeezy theme.json', (yargs) => {
+            yargs.positional('themeDir', {
+                type: 'string',
+                alias: 't',
+                default: './',
+                describe: 'Path to a Lemon Squeezzy theme directory'
+            });
+            yargs.positional('outputDir', {
+                type: 'string',
+                alias: 'o',
+                default: './',
+                describe: 'Path to an output directory for the generated theme.json'
+            });
+        }, async (argv) => {
+            const themeDir = path.resolve(process.cwd(), argv.themeDir);
+            const outputDir = path.resolve(process.cwd(), argv.outputDir);
+
+            const pkg = require(path.resolve(themeDir, 'package.json'));
+
+            let theme = {
+                id: pkg.name,
+                meta: {
+                    description: pkg.description || '',
+                },
+                settings: [],
+                wedges: [],
+                templates: []
+            };
+
+            const components = loadComponents(path.resolve(themeDir, 'wedges'));
+            theme.settings = componentToWedgeConfig(components, themeDir);
+
+            const output = JSON.stringify(theme, null, 4);
+
+            const themePath = path.resolve(outputDir, 'theme.json');
+            fs.writeFileSync(themePath, output);
+            console.log(chalk.green(`Wrote theme.json to ${themePath}`));
+        })
+        .help()
+        .argv
+})();
+
+function componentToWedgeConfig(components, themeDir) {
+    return components.map(component => {
+        return {
+            component: component.file.replace(themeDir, '').replace(/^\//, ''),
+            settings: component.props
+        }
+    });
+}
+
+function loadComponents(path) {
+    const files = glob.sync(`${path}/**/*.vue`);
+    return files.map(file => {
+        console.log(`Processing ${file}...`);
+        const content = fs.readFileSync(file).toString();
+        const parsed = compiler.parseComponent(content);
+
+        let props = {};
+        if (parsed.script) {
+            const script = moduleFromString.importFromStringSync(parsed.script.content);
+            props = script.default.props ? script.default.props : {};
+            props = typeof props === 'object' ? props : {}; // Arrays not supported
+
+            props = objectMap(props, (prop, key) => {
+                prop = typeof prop === 'object' ? prop : {}; // Arrays not supported
+                return transformProp(key, prop);
+            });
+            props = JSON.parse(JSON.stringify(props));
+        }
+
+        return {
+            file: file,
+            props: Object.values(props),
+        }
+    });
+}
+
+function transformProp(key, prop) {
+    prop.input = prop.input ? prop.input : {
+        type: 'text'
+    };
+
+    return {
+        id: key,
+        ...prop.input,
+    };
+}
+
+function objectMap(obj, fn) {
+    return Object.fromEntries(
+        Object.entries(obj).map(
+            ([k, v], i) => [k, fn(v, k, i)]
+        )
+    );
+}
